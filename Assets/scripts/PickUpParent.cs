f5bc787bcfdb8e4fe332567e612c9f25b6beadac
Eden JnBaptiste <Eden JnBaptiste>
2016-09-17 18:55:44 -0700

    Adding trigger input

---
+++ Assets/scripts/PickUpParent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickUpParent : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+    
+	void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void FixedUpdate () {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if(device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are touch holding the trigger");
+        }
+	}
+}
