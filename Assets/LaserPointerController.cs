b7e34464a62d44b3fd510703131427773c5019f3
Max Strini <mcstrini@gmail.com>
2016-09-17 17:14:20 -0700

    Add cat and laser pointer scripts

---
+++ Assets/LaserPointerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserPointerController : MonoBehaviour {
+    float radius = 9f;
+    Vector2 center;
+    float theta = 0f;
+    float periodInSec = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+       center  = new Vector2(this.transform.localPosition.x, this.transform.localPosition.z) - new Vector2(radius, 0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        theta += 2 * Mathf.PI * Time.deltaTime / periodInSec;
+        var pos2 = center + new Vector2(Mathf.Cos(theta), Mathf.Sin(theta)) * radius;
+        this.transform.localPosition = new Vector3(pos2.x, this.transform.localPosition.y, pos2.y);
+	}
+}
